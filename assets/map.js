0b7f233a310758fc353df4b2ab576a9566ab74b8
anshori <m.anshory@gmail.com>
2024-01-17 23:51:04 +0700

    First base code

---
+++ assets/map.js
@@ -0,0 +1,67 @@
+/* Initial Map */
+var map = L.map('map').setView([-7.7905952, 110.3756905], 13); //lat, long, zoom
+
+// disable attribution
+map.attributionControl.setPrefix(false);
+
+/* Tile Basemap */
+var street = L.tileLayer('https://mt0.google.com/vt/lyrs=r&hl=en&x={x}&y={y}&z={z}', {
+	maxZoom: 20,
+	minZoom: 0,
+	attribution: 'Google Street',
+	label: 'Google Street'
+});
+
+var satellite = L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+	maxZoom: 20,
+	minZoom: 0,
+	subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+	attribution: 'Google Satellite',
+	label: 'Google Satellite'
+});
+
+var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 20,
+	minZoom: 0,
+	attribution: 'OpenStreetMap',
+	label: 'OSM'
+});
+
+// Control Layer Basemaps
+map.addControl(L.control.basemaps({
+	basemaps: [osm, street, satellite],
+	tileX: 0, // tile X coordinate
+	tileY: 0, // tile Y coordinate
+	tileZ: 1 // tile zoom level
+}));
+
+/* GeoJSON Point */
+var pointobj = L.geoJson(null, {
+	onEachFeature: function (feature, layer) {
+		if (feature.properties) {
+			var content = "<strong class='mb-3'>" + feature.properties.nama + "</strong>" +
+				"<table class='table table-striped table-bordered'>" +
+				"<tr><td>Jenis Fasilitas</td><td>" + feature.properties.jenis_fasilitas + "</td></tr>" +
+				"<tr><td>Deskripsi</td><td>" + feature.properties.deskripsi + "</td></tr>" +
+				"<tr><td>Jumlah Guru Laki-laki</td><td>" + feature.properties.guru_laki + "</td></tr>" +
+				"<tr><td>Jumlah Guru Perempuan</td><td>" + feature.properties.guru_perempuan + "</td></tr>" +
+				"<tr><td>Jumlah Santri Laki-laki</td><td>" + feature.properties.santri_laki + "</td></tr>" +
+				"<tr><td>Jumlah Santri Perempuan</td><td>" + feature.properties.santri_perempuan + "</td></tr>" +
+				"</table>"
+			"<small>" + feature.properties.timestamp + "</small>";
+			layer.on({
+				click: function (e) {
+					pointobj.bindPopup(content);
+				},
+				mouseover: function (e) {
+					pointobj.bindTooltip(feature.properties.nama);
+				}
+			});
+		}
+	}
+});
+$.getJSON("https://script.google.com/macros/s/AKfycbw7i8CnaNnAGnSPKJeOB6s_Oz5X-BRnLi1vGJyappVg1jtJWx7suW3Jn3G5WbpvGFsN/exec", function (data) {
+	pointobj.addData(data);
+	map.addLayer(pointobj);
+	map.fitBounds(pointobj.getBounds());
+});
