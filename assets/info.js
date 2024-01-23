93d3455ff18cf6d6d40d6d43e63a95b43273c204
anshori <m.anshory@gmail.com>
2024-01-23 15:59:32 +0700

    Change title & info

--- assets/info.js
+++ assets/info.js
@@ -1,3 +1,3 @@
 function info() {
-	$('#content-info').html('Aplikasi ini digunakan untuk pengumpulan data aset PBNU Kota Yogyakarta');
+	$('#content-info').html('Aplikasi ini digunakan untuk pengumpulan data aset Pengurus Wilayah Nahdlatul Ulama (PWNU) Daerah Istimewa Yogyakarta');
 }
